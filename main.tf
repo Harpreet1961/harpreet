e8a86eefd9025dc8870e17cd76a3c250545dde52
Harpreet1961 <105535781+Harpreet1961@users.noreply.github.com>
2022-08-11 12:42:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ module "tf-connect-tgw" {
   ]
   vpc_id = data.aws_vpc.vpc_id.id
   tgw-attachment-name = var.tgw-attachment-name
-  transit_gateway_id = "tgw-0fcc34220e6eaa9e2"
+  transit_gateway_id = "tgw-0cdbeec6b482b9c53"
 }
 
 
